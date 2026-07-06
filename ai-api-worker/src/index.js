0e83f7bd7aa5560bdf9e5c7673616b5f9e4215d3
Narges Yousufzada <narges.yousofzada1@gmail.com>
2026-07-06 12:46:37 +0430

    Initial commit

---
+++ ai-api-worker/src/index.js
@@ -0,0 +1,62 @@
+import {OpenAI} from "openai";
+import { Responses } from "openai/resources/index.mjs";
+
+const corsHeaders = {
+	'Access-Control-Allow-Origin': '*',
+	'Access-Control-Allow-Methods': 'POST, OPTIONS',
+	'Access-Control-Allow-Headers': 'Content-Type',
+};
+
+export default {
+	async fetch(request, env, ctx) {
+		if (request.method === 'OPTIONS') {
+			return new Response(JSON.stringify(null), { status: 200, headers: corsHeaders });
+
+		}
+		if (request.method !== 'POST') {
+			return new Response(
+				JSON.stringify(
+					{
+						message: 'Method not allowed!',
+					},
+					{ status: 405, headers: corsHeaders },
+				),
+			);
+		}
+
+		try {
+       
+        const openai = new OpenAI({
+				apiKey: env.OPENAI_API_KEY,
+				baseURL: env.API_URL
+			  });
+              
+			  const data = await request.json();
+
+
+			  const res = await openai.chat.completions.create({
+				model: env.AI_MODEL,
+				messages: data,
+			  });
+
+			  
+
+		return new Response(JSON.stringify(res.choices[0].message), {
+			status: 200,
+			headers: corsHeaders,
+		});
+			} catch (error) {
+				return new Response(JSON.stringify({
+					message: "Something went wrong!" + error.message,
+
+				
+				}),
+				{
+					status: 500,
+					headers: corsHeaders
+				},
+			);
+				
+		}
+	},
+};
